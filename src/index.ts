f7d3d50a0be85965dec9506ee9b4703a126be74b
ghoshOFahan <ahanghosh031004@gmail.com>
2025-10-31 19:57:24 +0530

    fix:env variables for render

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import express from "express";
 const app = express();
 import "dotenv/config";
-const port = process.env.PORT;
+const port: number = parseInt(process.env.PORT || "4000", 10);
+const host = "0.0.0.0";
 app.get("/", (req, res) => {
   res.send(`<h1>hey there welcome to rst!</h1>`);
 });
-app.listen(port, () => {
-  console.log("you can access the page locally at http://localhost:%d", port);
+app.listen(port, host, () => {
+  if (process.env.port) {
+    console.log("service is live and listening on port%d", port);
+  } else {
+    console.log("you can access the page locally at http://localhost:%d", port);
+  }
 });
