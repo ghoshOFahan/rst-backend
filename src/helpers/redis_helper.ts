88d5805ebbcd87448bbb4302e797c51edb2a4b6b
ghoshOFahan <ahanghosh031004@gmail.com>
2025-12-17 12:52:12 +0530

    fix:fixed findWord bug

--- src/helpers/redis_helper.ts
+++ src/helpers/redis_helper.ts
@@ -111,8 +111,8 @@ export async function findWord(
 ): Promise<boolean> {
   try {
     const words = await redisClient.lrange(WORD_HISTORY_KEY(roomId), 0, -1);
-    return !words.includes(word.toLowerCase());
+    return words.includes(word.toLowerCase());
   } catch (error) {
-    return true;
+    return false;
   }
 }
